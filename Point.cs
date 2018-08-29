cc550b4d0b308635218ed327cd1785a9ef366464
Aaron Person <42382673+uzubastorm@users.noreply.github.com>
2018-08-29 11:23:12 -0400

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,45 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        private int x, y;
+        private static int objectCount = 0;
+
+        public Point()
+        {
+            this.x = -1;
+            this.y = -2;
+            Console.WriteLine("default constructor");
+            objectCount++;
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            objectCount++;  
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+        public double DistanceTo(Point other)
+        {
+            
+            int xDiff = this.x - other.x;
+            int yDiff = this.y - other.y;
+            double distance = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff));
+            Console.WriteLine($"the distance is {distance}");
+            return distance;
+            
+        }
+        public static int ObjectCount()
+        {
+           return(objectCount);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point();
+            Point bottomRight = new Point(1366, 768);
+            Point Ajay = new Point(77, 12);
+            double distance = origin.DistanceTo(bottomRight);
+            double distance2 = Ajay.DistanceTo(origin);
+            int oc = Point.ObjectCount();
+            Console.WriteLine($"the object count is {oc}");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
