1722d4d311a63cf89e819a337dae72bc0a872ddd
Aaron Person <42382673+uzubastorm@users.noreply.github.com>
2018-09-12 11:21:22 -0400

    Add files via upload

---
+++ ParamsArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class util
+    {
+        public static int Sum(params int[] paraList)
+        {
+            Console.WriteLine("using parameter list");
+            if(paraList == null)
+            {
+                throw new ArgumentException("util.sum: null parameter list");
+            }
+            if (paraList.Length == 0)
+            {
+                throw new ArgumentException("util.Sum: empty parameter list");
+            }
+            int sumtotal = 0;
+            foreach (int i in paraList)
+            {
+                sumtotal +=i;
+            }
+            return sumtotal;
+        }
+
+        public static int Sum(int param1 = 0, int param2 = 0, int param3 = 0, int param4 = 0)
+        {
+            Console.WriteLine("Using optional parameters");
+            int sumTotal = param1 + param2 + param3 + param4;
+            return sumTotal;
+        }
+    }
+}
---
+++ ParamsArray2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Program
+    {
+        static void doWork()
+        {
+            int sum = 0;
+                sum = util.Sum(10,9,8,4);
+            Console.WriteLine($"the sum is:{sum}");
+                
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
