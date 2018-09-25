64f5afcc649339a22ba0d79dd0e72937e918626b
Aaron Person <42382673+uzubastorm@users.noreply.github.com>
2018-09-25 10:21:04 -0400

    Add files via upload

---
+++ Drawingwithproperties.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Drawing
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DrawingPad : Page
+    {
+        public DrawingPad()
+        {
+            this.InitializeComponent();
+        }
+
+        private void drawingCanvas_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Point mouseLocation = e.GetPosition(this.drawingCanvas);
+            Square mySquare = new Square(100);
+
+            if (mySquare is IDraw)
+            {
+                IDraw drawSquare = mySquare;
+                drawSquare.X = (int)mouseLocation.X;
+                drawSquare.Y = (int)mouseLocation.Y;
+                drawSquare.Draw(drawingCanvas);
+            }
+
+            if (mySquare is IColor)
+            {
+                IColor colorSquare = mySquare;
+                colorSquare.Color = Colors.BlueViolet;
+            }
+        }
+        
+        private void drawingCanvas_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            Point mouseLocation = e.GetPosition(this.drawingCanvas);
+            Circle myCircle = new Circle(100);
+
+            if (myCircle is IDraw)
+            {
+                IDraw drawCircle = myCircle;
+                drawCircle.X = (int)mouseLocation.X;
+                drawCircle.Y = (int)mouseLocation.Y;
+                drawCircle.Draw(drawingCanvas);
+            }
+
+            if (myCircle is IColor)
+            {
+                IColor colorCircle = myCircle;
+                colorCircle.Color = Colors.HotPink;
+            }
+        }
+    }
+}
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Drawing
+{
+    abstract class DrawingShape
+    {
+        protected int _size;
+        protected int _x = 0, _y = 0;
+        protected Shape shape = null;
+
+        public DrawingShape(int size)
+        {
+            this._size = size;
+        }
+
+        public int X
+        {
+            get { return this._x; }
+            set { this._x = value; }
+        }
+
+        public int Y
+        {
+            get { return this._y; }
+            set { this._y = value; }
+        }
+               
+
+        public Color Color
+        {
+            set
+            {
+                if (this.shape != null)
+                {
+                    SolidColorBrush brush = new SolidColorBrush(value);
+                    this.shape.Fill = brush;
+                }
+            }
+        }
+                
+        public virtual void Draw(Canvas canvas)
+        {
+            if(this.shape == null)
+            {
+                throw new InvalidOperationException("Shape is null");
+            }
+
+            this.shape.Height = this._size;
+            this.shape.Width = this._size;
+            Canvas.SetTop(this.shape, this._y);
+            Canvas.SetLeft(this.shape, this._x);
+            canvas.Children.Add(this.shape);
+        }
+    }    
+}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace Drawing
+{
+    interface IColor
+    {
+        Color Color { set; }
+    }
+}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Drawing
+{
+    interface IDraw
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        void Draw(Canvas canvas);
+    }
+}
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Selection
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void compareClick(object sender, RoutedEventArgs e)
+        {
+            int diff = dateCompare(firstDate.Date.LocalDateTime, secondDate.Date.LocalDateTime);
+            info.Text = "";
+            show("firstDate == secondDate", diff == 0);
+            show("firstDate != secondDate", diff != 0);
+            show("firstDate <  secondDate", diff < 0);
+            show("firstDate <= secondDate", diff <= 0);
+            show("firstDate >  secondDate", diff > 0);
+            show("firstDate >= secondDate", diff >= 0);
+        }
+
+        private void show(string exp, bool result)
+        {
+            info.Text += exp;
+            info.Text += " : " + result.ToString();
+            info.Text += "\n";
+        }
+
+        private int dateCompare(DateTime leftHandSide, DateTime rightHandSide)
+        {
+      
+           
+            int result = 0;
+
+            if (leftHandSide.Year < rightHandSide.Year)
+            {
+                result = -18;
+            }
+            else if (leftHandSide.Year > rightHandSide.Year)
+            {
+                result = 18;
+            }
+            else if (leftHandSide.Month < rightHandSide.Month)
+            {
+                result = -667;
+            }
+            else if (leftHandSide.Month > rightHandSide.Month)
+            {
+                result = 1953;
+            }
+            else if (leftHandSide.Day < rightHandSide.Day)
+            {
+                result = -90;
+            }
+            else if (leftHandSide.Day > rightHandSide.Day)
+            {
+                result = 76;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+    }
+}
+
